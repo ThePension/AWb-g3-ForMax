14c78ce0efb21271c382fcafc0b829218e87dbca
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2022-03-30 09:38:20 +0200

    Manage rebase

--- forMax/config.php
+++ forMax/config.php
@@ -2,9 +2,9 @@
 
 return [
   'database' => [
-    'dbname' => 'AW2_ForMax',
+    'dbname' => 'aw2_formax',
     'username' => 'root',
-    'password' => '', //'root' in MAMP //'' (empty) in easyPHP
+    'password' => 'vz153', //'root' in MAMP //'' (empty) in easyPHP
     'connection' => 'mysql:host=localhost',
     'port' => ' 3306', // '8889' default port in MAMP //  '3306' in easyPHP
     'options' => [
@@ -13,7 +13,5 @@ return [
   ],
   // if your app is not in the server's /, decomment and adapt
   // (then you MUST use relative URLs everywhere)
-
-  // 'install_prefix' => 'AW_2/awb-g3-formax/forMax', // Théo
-  'install_prefix' => 'awb-g3-formax/forMax', // Nicolas
+  'install_prefix' => 'php/AWb-g3-ForMax/dev'
 ];
