588d1783f565162ce516237e53814e123ff100a0
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2022-04-06 09:23:52 +0200

    Adding bootstrap grid view for topic

--- forMax/app/models/Topic.php
+++ forMax/app/models/Topic.php
@@ -19,4 +19,44 @@ class Topic
     {
         return $this->property;
     }
+
+    public function getAsBootstrapGridForHomePage()
+    {
+        $install_prefix = App::get('config')['install_prefix'];
+
+        $pathToTheTopic = urlencode("/" + $install_prefix + "/topic_show?id=" + $this->id);
+
+        $topicHtml = 
+            "<div class='card'>
+                <div class='card-header'>"
+                +
+                    htmlentities($this->name);
+                +
+                "</div>
+                <div class='card-body'>
+                    <p class='card-text text-truncate'>"
+                    +
+                        htmlentities($this->content);
+                    +
+                    "</p>
+                    <a href='/"+ $pathToTheTopic +"' class='btn btn-info'>Read further</a>
+                    <p>created on "
+                    +
+                        htmlentities($this->creation_timestamp);
+                    +
+                    " | updated on "
+                    +
+                        htmlentities($this->update_timestamp);
+                    +
+                    "</p>
+                </div>
+            </div>";
+
+        return $topicHtml;
+    }
+
+    public function getAsBootstrapGridForTopicPage()
+    {
+
+    }
 }
