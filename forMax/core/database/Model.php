3f3bca22bc0b31e72d761f6e6336f1c90b284cbd
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2022-04-20 15:21:06 +0200

    little code review

--- forMax/core/database/Model.php
+++ forMax/core/database/Model.php
@@ -138,28 +138,3 @@ abstract class Model
 
     }
 }
-
-// old version
-/*
-protected static function fetchAll($table, $intoClass)
-{
-    $statement = App::get('dbh')->prepare("select * from {$table}");
-    $statement->execute();
-    return $statement->fetchAll(PDO::FETCH_CLASS, $intoClass);
-}
-
-protected static function fetchById($id, $table, $intoClass)
-{
-    // ASSUMPTION
-    //    - $id was validated by the caller
-
-    $statement = App::get('dbh')->prepare("select * from {$table} where id=:row_id");
-    $statement->bindParam(':row_id', $id);
-    $statement->setFetchMode(PDO::FETCH_CLASS, $intoClass);
-    $statement->execute();
-    return $statement->fetch();
-}
-
-// abstract non static implementation
-abstract protected function save();
-*/
