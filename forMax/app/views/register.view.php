f037adc5d8cb5f3455fbc1dd84ac8e98854fafde
nicolas.aubert1 <nicolas.aubert1@hes-so.ch>
2022-05-04 08:38:24 +0200

    Adding register form (issue #12)

---
+++ forMax/app/views/register.view.php
@@ -0,0 +1,45 @@
+<?php
+    $title = "Register";
+    require('partials/header.php')
+?>
+
+<!-- Based on https://mdbootstrap.com/docs/standard/extended/registration/ -->
+
+<div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div class="card" style="border-radius: 15px;">
+                    <div class="card-body p-5">
+                        <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+                        <form method="POST" action="/<?= $install_prefix ?>/register_do">
+                            <div class="form-outline mb-4">
+                                <input type="text" id="username" name="username" class="form-control form-control-lg" />
+                                <label class="form-label" for="username">Username</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="password" name="password" class="form-control form-control-lg" />
+                                <label class="form-label" for="password">Password</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="password" password="password" class="form-control form-control-lg" />
+                                <label class="form-label" for="password">Repeat your password</label>
+                            </div>
+
+                            <div class="d-flex justify-content-center">
+                                <button type="button" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                            </div>
+
+                            <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="/<?= $install_prefix ?>/login" class="fw-bold text-body"><u>Login here</u></a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require('partials/footer.php') ?>
