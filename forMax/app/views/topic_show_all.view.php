894aabfa08be707bba87d1892dba97139caf8e2f
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2022-05-22 21:48:56 +0200

    #39 Fixing responsive in topics page

--- forMax/app/views/topic_show_all.view.php
+++ forMax/app/views/topic_show_all.view.php
@@ -23,7 +23,7 @@ require('partials/header.php');
 		}
 		?>
 		
-		<div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4">
+		<div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
 			<?php
 			foreach ($topics as $topic)
 			{
