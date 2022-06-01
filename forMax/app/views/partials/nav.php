4c4b8e03e18a95dfb602cdf38fe4a6e1c4e9586a
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2022-06-01 10:30:05 +0200

    #55 Adding logo

--- forMax/app/views/partials/nav.php
+++ forMax/app/views/partials/nav.php
@@ -2,10 +2,14 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/<?= Helper::createUrl("index") ?>">Home</a>
+    <a class="navbar-brand" href="/<?= Helper::createUrl("index") ?>">
+      <img src="/<?= Helper::createUrl("public/images/formax_logo.svg")?>" alt="ForMax Logo" height="48" class="d-inline-block ms-3 me-3 align-text-top">
+    </a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
